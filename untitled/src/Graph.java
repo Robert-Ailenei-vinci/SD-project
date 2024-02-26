e7eac7651dd5ccf190f62bb3b181870ef260f8f7
rafael-millor-vinci <rafael.millor@student.vinci.be>
2024-02-26 14:55:28 +0100

    add

--- untitled/src/Graph.java
+++ untitled/src/Graph.java
@@ -1,2 +1,14 @@
 public class Graph {
+
+
+  public String calculerItineraireMinimisantNombreRoutes (String  start, String finnish  ){
+
+        return null;
+    }
+
+
+    public String calculerItineraireMinimisantKm (String  start, String finnish  ){
+
+        return null;
+    }
 }
